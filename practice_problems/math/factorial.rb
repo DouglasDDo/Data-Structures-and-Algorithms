76fc626eb4d579f7f67f5e4d3d5071ec5ce21c92
DouglasDDo <dougddo@gmail.com>
2017-02-09 22:22:35 -0800

    Adds new problems for later.

--- practice_problems/math/factorial.rb
+++
@@ -1,6 +0,0 @@
-# Objective: Return factorial n (n!)
-
-def factorial(n)
-  return nil unless n.is_a?(Integer)
-  n <= 1 ? 1 : (1..n).reduce(:*)
-end
--- practice_problems/math/is_prime.rb
+++
@@ -1,7 +0,0 @@
-# Objective: Determine if a given number is prime
-
-def is_prime?(num)
-  return false if num < 2
-
-  (2 .. num/2).none? { |n| num % n == 0 }
-end
