599591c0bcb6ee7715b32ef19aa953482818315b
DouglasDDo <dougddo@gmail.com>
2017-03-21 13:06:49 -0700

    Corrects objective text.

--- algorithms/dynamic_programming/ruby/word_break.rb
+++ algorithms/dynamic_programming/ruby/word_break.rb
@@ -1,6 +1,6 @@
 # Objective: Given a string input and a dictionary of words, if the word is entirely
 # composed of words from the dictionary, return a space-separated string of words;
-# Otherwise, return nil. Use recursive backtracking to arrive at the solution.
+# Otherwise, return nil. Use dynamic programming to arrive at the solution.
 
 def word_break(str, dict)
   # Cover invalid arguments
