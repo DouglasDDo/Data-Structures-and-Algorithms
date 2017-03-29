9f58bd24052e4e726cdd9fe9fd585f68670b807d
DouglasDDo <dougddo@gmail.com>
2017-03-29 16:51:39 -0700

    Makes minor changes for less setup.

--- practice_problems/math/ruby/fibonacci_dyn.rb
+++ practice_problems/math/ruby/fibonacci_dyn.rb
@@ -3,13 +3,12 @@
 def fib_dyn(n)
   # Create a memo to hold values
   result = Array.new
-  # Set the 0th value
-  result[0] = 0
   # Set the first two fib values
-  result[1] = result[2] = 1
+  result[0] = 0
+  result[1] = 1
 
-  # Fill out the memo with numbers from 3 to n - 1
-  (3 ... n).each do |i|
+  # Fill out the memo with numbers from 2 to n
+  (2 .. n).each do |i|
     # For each index, the value will be the sum of the previous two
     result[i] = result[i - 1] + result[i - 2]
   end
